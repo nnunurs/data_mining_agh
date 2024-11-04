1f5c27e9ea251052c0aee02f70aa5fcc551b956b
Michał Truszel <33067782+nnunurs@users.noreply.github.com>
2024-11-04 20:34:55 +0100

    Modify comparison for better visualization of accuracy

--- comparison.py
+++ comparison.py
@@ -3,7 +3,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from decision_tree import DecisionTreeClassifier as OurDecisionTree
 from sklearn.metrics import accuracy_score
+from sklearn.datasets import fetch_openml
 import matplotlib.pyplot as plt
+import numpy as np
 
 iris = load_iris()
 X_iris, y_iris = iris.data, iris.target
@@ -17,6 +19,12 @@ X_train_wine, X_test_wine, y_train_wine, y_test_wine = train_test_split(
     X_wine, y_wine, test_size=0.2
 )
 
+mnist = fetch_openml('mnist_784', version=1, as_frame=False)
+X_mnist, y_mnist = mnist.data, mnist.target
+subset_size = int(0.01 * len(X_mnist))  # 10% z całego zbioru
+indices = np.random.choice(len(X_mnist), subset_size, replace=False)
+X_subset, y_subset = X_mnist[indices], y_mnist[indices]
+X_train_mnist, X_test_mnist, y_train_mnist, y_test_mnist = train_test_split(X_mnist, y_mnist, test_size=0.2, random_state=42)
 
 results_ours = {}
 results_builtin = {}
@@ -51,26 +59,60 @@ for depth in range(1, MAX_DEPTH):
 
 plt.plot(list(results_ours.keys()), list(results_ours.values()), label="Ours")
 plt.plot(list(results_builtin.keys()), list(results_builtin.values()), label="Builtin")
+plt.xlabel("Tree Depth")
+plt.ylabel("Accuracy")
+plt.title("Comparison of Decision Tree Classifiers on Iris Dataset")
+plt.legend()
 plt.show()
 
-# plt.figure(figsize=(12, 8))
-# tree.plot_tree(dt_iris, filled=True, feature_names=iris.feature_names, class_names=iris.target_names)
-# plt.title("Decision Tree - Iris")
-# plt.show()
+results_ours = {}
+results_builtin = {}
 
-# #wine
+for depth in range(1, MAX_DEPTH):
+    print(f"Depth: {depth}")
+    results_ours[depth] = run(
+        depth, OurDecisionTree, X_train_wine, X_test_wine, y_train_wine, y_test_wine
+    )
+    results_builtin[depth] = run(
+        depth,
+        DecisionTreeClassifier,
+        X_train_wine,
+        X_test_wine,
+        y_train_wine,
+        y_test_wine,
+    )
+
+plt.plot(list(results_ours.keys()), list(results_ours.values()), label="Ours")
+plt.plot(list(results_builtin.keys()), list(results_builtin.values()), label="Builtin")
+plt.xlabel("Tree Depth")
+plt.ylabel("Accuracy")
+plt.title("Comparison of Decision Tree Classifiers on Wine Dataset")
+plt.legend()
+plt.show()
 
 
-# dt_wine = DecisionTreeClassifier()
-# dt_wine.fit(X_train_wine, y_train_wine)
 
-# y_pred_wine = dt_wine.predict(X_test_wine)
+results_ours_mnist = {}
+results_builtin_mnist = {}
 
-# accuracy_wine = accuracy_score(y_test_wine, y_pred_wine)
-# results["wine"] = accuracy_wine
-# print(f"Wine Accuracy: {accuracy_wine:.2f}")
+for depth in range(1, MAX_DEPTH):
+    print(f"Depth: {depth}")
+    results_ours_mnist[depth] = run(
+        depth, OurDecisionTree, X_train_mnist, X_test_mnist, y_train_mnist, y_test_mnist
+    )
+    results_builtin_mnist[depth] = run(
+        depth,
+        DecisionTreeClassifier,
+        X_train_mnist,
+        X_test_mnist,
+        y_train_mnist,
+        y_test_mnist,
+    )
 
-# plt.figure(figsize=(12, 8))
-# tree.plot_tree(dt_wine, filled=True, feature_names=wine.feature_names, class_names=wine.target_names)
-# plt.title("Decision Tree - Wine")
-# plt.show()
+plt.plot(list(results_ours_mnist.keys()), list(results_ours_mnist.values()), label="Ours")
+plt.plot(list(results_builtin_mnist.keys()), list(results_builtin_mnist.values()), label="Builtin")
+plt.xlabel("Tree Depth")
+plt.ylabel("Accuracy")
+plt.title("Comparison of Decision Tree Classifiers on MNIST Dataset")
+plt.legend()
+plt.show()
